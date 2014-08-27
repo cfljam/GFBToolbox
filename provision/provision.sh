a2b3591c1a141a88fdd7625cd23931095e34a088
cfljam <john.mccallum@plantandfood.co.nz>
2014-08-27 13:39:35 +1200

    minor changes

--- provision/provision.sh
+++ provision/provision.sh
@@ -2,14 +2,15 @@
 sudo apt-get -y update
 sudo apt-get -y install  curl build-essential python-dev python-pip  python-numpy python-scipy git
 
-sudo pip install --upgrade 
+sudo pip install --upgrade
 sudo easy_install pyzmq jinja2 numpy tornado pandas scipy distribute
 sudo pip install --upgrade ipython[all]
 
 ## Primer design pre-requisites
 sudo easy_install Biopython bcbio-gff
 sudo apt-get -y install primer3
-## Gisting
+
+## set up for Gisting
 sudo apt-get -y install rubygems
 sudo gem install gist
 
@@ -17,5 +18,6 @@ sudo gem install gist
 sudo apt-get -y install screen
 curl -o ~/.screenrc https://gist.githubusercontent.com/cfljam/610e9fe0a8ca23e80199/raw
 
-### R base
-#sudo apt-get -y install r-base-core
+### install R base and R2Py
+# sudo apt-get -y install r-base-dev
+# sudo pip install rpy2
